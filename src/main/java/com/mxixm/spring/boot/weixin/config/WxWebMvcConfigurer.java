77877316bc58e20c3f02e80aaf759cc81d81a792
光闪 <guangshan1992@qq.com>
2022-07-22 15:32:38 +0800

    Update WxWebMvcConfigurer.java

--- src/main/java/com/mxixm/spring/boot/weixin/config/WxWebMvcConfigurer.java
+++ src/main/java/com/mxixm/spring/boot/weixin/config/WxWebMvcConfigurer.java
@@ -2,6 +2,7 @@ package com.mxixm.spring.boot.weixin.config;
 
 import com.mxixm.spring.boot.weixin.core.WxArgumentResolver;
 import com.mxixm.spring.boot.weixin.core.message.WxMessageReturnValueHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
@@ -21,6 +22,12 @@ public class WxWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> handlers) {
         // 添加返回值处理器
-        handlers.add(new WxMessageReturnValueHandler());
+        handlers.add(wxMessageReturnValueHandler());
     }
+
+    @Bean
+    public WxMessageReturnValueHandler wxMessageReturnValueHandler() {
+        return new WxMessageReturnValueHandler();
+    }
+
 }
